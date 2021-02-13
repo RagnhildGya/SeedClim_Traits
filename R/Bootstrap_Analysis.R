df332c660cd599eaf1a927a983b92a9d92077f6b
Ragnhild Gya <rgy042@uib.no>
2021-02-13 13:58:44 +0100

    Adding code for no taxnonmy bootstrapping mode output

--- R/Bootstrap_Analysis.R
+++ R/Bootstrap_Analysis.R
@@ -452,10 +452,12 @@ model_output_linear <-function(dat) {
 
 
 model_output_time_mixed <- model_output_mixed(tidy_time_model_predicted_mixed)
+model_output_time_mixed_notax <- model_output_mixed(tidy_time_model_predicted_mixed_notax)
 #model_output_time_linear <- model_output_linear(tidy_time_model_predicted_linear)
 model_output_com_time_mixed <- model_output_com_mixed(tidy_com_time_model_predicted_mixed)
 
 model_output_space_mixed <- model_output_mixed(tidy_space_model_predicted_mixed)
+model_output_space_mixed_notax <- model_output_mixed(tidy_space_model_predicted_mixed_notax)
 #model_output_space_linear <- model_output_linear(tidy_space_model_predicted_linear)
 model_output_com_space_mixed <- model_output_com_mixed(tidy_com_space_model_predicted_mixed)
 
